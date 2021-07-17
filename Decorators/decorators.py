a72b9b35faa65f1f4ffc505e99c150b850eb90c1
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-07-17 22:28:56 +0530

    Update decorators.py

--- Decorators/decorators.py
+++ Decorators/decorators.py
@@ -4,19 +4,19 @@ from time import perf_counter
 
 
 
-def odd_it(fn: "Function"):
+def odd_it(fn: function) -> function:
 	"""This Decorator function allows to run a function only at odd seconds, else prints out "We're even!"""
-	def inner(*args, **kwargs):
+	def inner(*args, **kwargs) -> function:
 		if datetime.now().second % 2 == 1:
 			return fn(*args, **kwargs)
 		print("We're even!")
 	return inner
 
-def logger(fn: "Function"):
+def logger(fn: function) -> function:
 	"""This Decorator function logs the time at which the function is called, the description (docstring) of the called function and the annotations of the function."""
 	import datetime, inspect	
 	@wraps(fn)
-	def inner(*args, **kwargs):
+	def inner(*args, **kwargs) -> function:
 		print(f"Function Name {fn.__name__} was called at {datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=5.5)))}")
 		print(f"Function description : {fn.__doc__}")
 		print(f"Function annotation : {inspect.signature(fn)}")
@@ -28,12 +28,12 @@ def logger(fn: "Function"):
 	return inner
 
 
-def decorator_factory(access:str):
+def decorator_factory(access:str) -> function:
 	"""This Decorator factory function provides access to variables based upon the access level type passed as input to the decorator factory function."""
-	def outer(fn): 
+	def outer(fn: function) -> function: 
 		var1, var2, var3, var4 = 0, 0, 0, 0
 		@wraps(fn)
-		def inner():
+		def inner() -> function:
 			if access == 'high':
 				return [var1, var2, var3, var4]
 			elif access == 'mid':
@@ -49,11 +49,11 @@ def decorator_factory(access:str):
 
 
 
-def authenticate(set_password):
+def authenticate(set_password: str) -> function:
 	"""This Decorator function runs the function inside it only if the password that is passed as an input to the actual function is the same as the password that is set through the input to the decorator function."""
-	def outer(fn):
+	def outer(fn: function) -> function:
 		@wraps(fn)
-		def inner(arg):
+		def inner(arg: str) -> function:
 			if set_password == arg:
 				return fn()
 			else:
@@ -63,11 +63,11 @@ def authenticate(set_password):
 
 
 # The timing function
-def timed(reps):
+def timed(reps: int) -> function:
 	"""This decorator function takes an input to it that specifies the amount of times a function is run. It then runs the actual function that many number of times and calculates the overall time taken over all the iterations and also the average time taken for each iteration of the function to run."""
-	def outer(fn):
+	def outer(fn: function) -> function:
 		@wraps(fn)
-		def inner(*args, **kwargs):
+		def inner(*args, **kwargs) -> function:
 			end = 0
 			for i in range(reps):
 				start = perf_counter()
