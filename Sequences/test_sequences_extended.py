798645ecddba92b2798ff89a6f2cfb06c95fb729
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-07-19 23:12:13 +0530

    Delete Sequences directory

--- Sequences/test_sequences_extended.py
+++
@@ -1,8 +0,0 @@
-from sequences_extended import Polygon_Sequence
-import pytest
-
-def test_sequences_properties():
-    polygon = Polygon_Sequence(4, 4)
-    assert len(polygon) == 2, "Length of Polygon_Sequence class has to be equal to the number of Polygons inside it."
-    assert polygon[1].num_vertices == 4, "Largest Polygon in the sequence must have as many vertices as the num_vertices_largest parameter argument value in the Polygon_Sequence class."
-    
