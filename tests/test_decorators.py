376e0217fb9add491a01f3a9c23ebd8206d78299
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-07-19 23:09:03 +0530

    Update test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -17,19 +17,19 @@ README_CONTENT_CHECK_FOR = [
     "timed"
 ]
 
-path = "/decorators"
+
 def test_readme_exists():
-    assert os.path.isfile("README.md"), "README.md file missing!"
+    assert os.path.isfile("/decorators/README.md"), "README.md file missing!"
 
 def test_readme_contents():
-    readme = open("README.md", "r")
+    readme = open("/decorators/README.md", "r")
     readme_words = readme.read().split()
     readme.close()
     assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
 def test_readme_proper_description():
     READMELOOKSGOOD = True
-    f = open("README.md", "r", encoding="utf-8")
+    f = open("/decorators/README.md", "r", encoding="utf-8")
     content = f.read()
     f.close()
     for c in README_CONTENT_CHECK_FOR:
@@ -39,7 +39,7 @@ def test_readme_proper_description():
     assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
 
 def test_readme_file_for_formatting():
-    f = open("README.md", "r", encoding="utf-8")
+    f = open("/decorators/README.md", "r", encoding="utf-8")
     content = f.read()
     f.close()
     assert content.count("#") >= 10
