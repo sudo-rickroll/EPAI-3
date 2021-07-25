683d5213cbf23cd9d84b95265a933a057054717a
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-07-26 00:13:43 +0530

    Update test_iterators.py

--- tests/test_iterators.py
+++ tests/test_iterators.py
@@ -4,12 +4,12 @@ def test_iter_exhaustion():
 
     polygon_obj = Polygon_Sequence_Iterable(5,2)
     
-    # Test 1
+    # Test Case 1
     assert "__iter__" in dir(polygon_obj), "Iterable class must contain the __iter__ method to be converted to an iterator."
     
     iter_obj = iter(polygon_obj)    
     for i in iter_obj:
         pass
     
-    # Test 2
+    # Test Case 2
     assert next(iter_obj) is not None, "Iterator has to be non-exhaustible."
