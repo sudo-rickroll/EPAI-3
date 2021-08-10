7a87cc1084e36c4cc140fd8db72e98f32b6592d6
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-08-10 22:29:45 +0530

    Add files via upload

---
+++ iterators/generators.py
@@ -0,0 +1,23 @@
+from datetime import date
+from dateutil import parser
+from typing import Generator
+from collections import namedtuple, Counter
+
+def date_parser(date_obj: object) -> date:
+    return parser.parse(date_obj).date()
+
+def cars_iter(file_loc: str) -> Generator:
+    with open(file_loc, encoding='utf8', errors='ignore') as f:
+        fields = next(f)
+        Cars = namedtuple("Cars", fields.replace(" ", ""))
+        types = [int, str, str, str, date_parser, int, str, str, str]
+        for row in f:
+            yield Cars(*[data_type(value) for data_type, value in zip(types, row.strip("\n").split(","))])
+
+def violations_made(cars_obj: object, *models):
+    counter = Counter(item.VehicleMake.lower() for item in sorted(cars_obj, key=lambda x : x.VehicleMake) if item.ViolationDescription != "")
+    for model in models:
+        print(f"{model} : {counter[model.lower()]}") if model.lower() in counter else print("Model not present in CSV file.")
+
+
+    
