c388571851fb0e4e5382ac4f4877d7a259f21646
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-08-10 22:32:05 +0530

    Update generators.py

--- iterators/generators.py
+++ iterators/generators.py
@@ -14,7 +14,7 @@ def cars_iter(file_loc: str) -> Generator:
         for row in f:
             yield Cars(*[data_type(value) for data_type, value in zip(types, row.strip("\n").split(","))])
 
-def violations_made(cars_obj: object, *models):
+def violations_count(cars_obj: object, *models):
     counter = Counter(item.VehicleMake.lower() for item in sorted(cars_obj, key=lambda x : x.VehicleMake) if item.ViolationDescription != "")
     for model in models:
         print(f"{model} : {counter[model.lower()]}") if model.lower() in counter else print("Model not present in CSV file.")
