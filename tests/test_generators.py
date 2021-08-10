af4e81504d9ffce4c987f9aed0c1b47a4e705810
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-08-10 22:34:30 +0530

    Update test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,15 +1,16 @@
 from typing import Generator, Iterator
 import io
 import sys
-from iterators import cars_iter, violations_made 
+from iterators import cars_iter, violations_count 
 
-cars_obj = cars(r"C:\Users\Ranga\OneDrive\Desktop\nyc_parking_tickets_extract-1.csv")
+cars_obj = cars_iter(r"../resources/nyc_parking_tickets_extract.csv")
+
 def test_return_type():        
     assert isinstance(cars_obj, (Iterator, Generator)), TypeError("Object must be an iterator")
 
 def test_violations_count():
     capturedOutput = io.StringIO()          
     sys.stdout = capturedOutput                   
-    violations_made(cars_obj, "NISSA")                                 
+    violations_count(cars_obj, "NISSA")                                 
     sys.stdout = sys.__stdout__                   
     assert "59" in capturedOutput.getvalue(), Exception("Wrong count of car model violations")
