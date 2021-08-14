f9c15eafd4a956f203ad2ebf6d14b7318cbf6824
Rangasai <65642947+sudo-rickroll@users.noreply.github.com>
2021-08-14 19:58:49 +0530

    corrections in context_managers module

--- context_managers/context_managers.py
+++ context_managers/context_managers.py
@@ -2,24 +2,14 @@ from datetime import date
 from dateutil import parser
 from typing import Generator
 from collections import namedtuple, Counter
-from contextlib import contextmanager
 from iterators import date_parser
 import builtins
 
-@contextmanager
-def csv_iter(entity_name, file_loc: str, *args) -> Generator:
-    try:
-        with open(file_loc, encoding='utf8', errors='ignore') as f:
-            rows = csv.reader(f, delimiter=',', quotechar='"')
-            fields = next(rows)
-            locals()[entity_name] = namedtuple(entity_name, fields)
-            for row in rows:
-                yield locals()[entity_name](*[getattr(builtins, data_type)(value) for data_type, value in zip(args, row) if data_type in builtins else locals()[data_type](value)])
 
-    finally:
-        del locals()[entity_name]
-
-a = csv_iter("Employment", "resources/employment.csv", "str", "str", "str", "str")
-for row in a:
-    print(row)
-    break
+def csv_iter(entity_name, file_loc: str, *args) -> Generator:    
+    with open(file_loc, encoding='utf8', errors='ignore') as f:
+        rows = csv.reader(f, delimiter=',', quotechar='"')
+        fields = next(rows)
+        locals()[entity_name] = namedtuple(entity_name, fields)
+        for row in rows:
+            yield locals()[entity_name](*[getattr(builtins, data_type)(value) if hasattr(builtins, data_type) else locals()[data_type](value) for data_type, value in zip(args, row)])
